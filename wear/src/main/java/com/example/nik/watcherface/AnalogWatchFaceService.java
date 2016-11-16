c04dbb6c89869e6361415f1635b4f799406554e9
Nikhil B <nkl07ba@gmail.com>
2016-11-16 14:20:09 +0530

    Initial commit

---
+++ wear/src/main/java/com/example/nik/watcherface/AnalogWatchFaceService.java
@@ -0,0 +1,14 @@
+package com.example.nik.watcherface;
+
+import android.support.wearable.watchface.CanvasWatchFaceService;
+
+/**
+ * Created by nik on 11/16/2016.
+ */
+
+public class AnalogWatchFaceService extends CanvasWatchFaceService {
+
+
+
+
+}
